aa0284c506d1587185bb64c22aa23ef3d86e7eef
Version_2.0 | Aditya Verma <105535211+Greatversion@users.noreply.github.com>
2025-05-07 14:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-# app.py
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
 import cv2
-import base64
+import os
+import psutil
 from utils import extract_hand_landmarks
 
 app = Flask(__name__)
@@ -12,6 +12,12 @@ app = Flask(__name__)
 model_dict = pickle.load(open('./model.p', 'rb'))
 model = model_dict['model']
 labels_dict = {0: "hello", 1: "no", 2: "yes"}
+
+# Function to log memory usage
+def log_memory_usage():
+    process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
+    print(f"Memory usage: {memory_info.rss / 1024 ** 2} MB")
 
 @app.route('/')
 def index():
@@ -26,6 +32,12 @@ def predict():
     image_np = np.frombuffer(file.read(), np.uint8)
     image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
 
+    # Resize the image to 640x480 to reduce memory and processing time
+    image = cv2.resize(image, (640, 480))
+    
+    log_memory_usage()  # Log memory usage after resizing
+
+    # Extract hand landmarks
     landmarks = extract_hand_landmarks(image)
 
     if landmarks:
